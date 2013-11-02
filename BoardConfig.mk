acb9dc92b9720f8fcde599300f1b2bec02e090dd
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-02 14:12:16 +0000

    Don't include the common config twice...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,8 +20,6 @@
 -include device/samsung/smdk4412-common/BoardCommonConfig.mk
 -include device/samsung/smdk4412-qcom-common/BoardCommonConfig.mk
 
--include device/samsung/t0lte/BoardCommonConfig.mk
-
 # inherit from the proprietary version
 -include vendor/samsung/t0lte/BoardConfigVendor.mk
 -include vendor/samsung/t0lteatt/BoardConfigVendor.mk
